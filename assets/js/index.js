0c6b046edfb0c3bef7dee5dbea3d8a9721ef1114
Ruslan Feduleyev <8reflections@gmail.com>
2023-07-23 19:14:59 +0600

    Last changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const loader = document.querySelector('.loader')
   setTimeout(() => loader.classList.add('loaderHidden'), 300)
 
+  AOS.init({
+    once: true,
+  });
+
 })
